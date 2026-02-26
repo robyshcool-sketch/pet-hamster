eb9fcb2b32a96194f9f0296a39cc830ff1db0cf2
robyshcool-sketch <robyshcool@gmail.com>
2026-02-26 11:14:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Sad)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.Asleep)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.UntilDone)
+    basic.showIcon(IconNames.Asleep)
+})
+basic.showIcon(IconNames.Asleep)
